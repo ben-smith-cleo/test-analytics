8899ac709a6ac454ed68020aa3099b2681319580
Benjamin Smith <ben.s@ben-smiths-mac.home>
2024-11-12 17:26:32 +0000

    adding queries and building

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="test_analytics",
-    version="0.1.0",
+    version="0.1.1",
     author="Ben Smith",
     author_email="ben.s@meetcleo.com",
     description="A test analytics package",
     packages=find_packages(),
     install_requires=['numpy', 'pandas'],
+    include_package_data=True,
+    package_data={"test_analytics": ["queries/*.sql"]}
 )
